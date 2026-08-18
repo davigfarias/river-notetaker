1756470e36ab9752c917ac24c47e4babfeff4a7d
davigfarias <seto.davigf@gmail.com>
2026-08-18 01:08:53 -0300

    String to text correction migration

---
+++ database/migrations/2026_08_18_010725_change_impressions_and_life_experiences_to_text_in_notes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('notes', function (Blueprint $table) {
+            $table->text('impressions')->nullable()->change();
+            $table->text('life_experiences')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('notes', function (Blueprint $table) {
+            $table->string('impressions')->nullable()->change();
+            $table->string('life_experiences')->nullable()->change();
+        });
+    }
+};
